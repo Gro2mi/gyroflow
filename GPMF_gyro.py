142ad45f05cb76ddb902f1c531ccf5e743e21193
Markus Rampp <mail@markusrampp.eu>
2021-11-10 08:30:57 +0100

    fixed gyro parsing

--- GPMF_gyro.py
+++ GPMF_gyro.py
@@ -85,7 +85,7 @@ class Extractor:
 
         # self.gyro_rate = round(self.num_gyro_samples / self.video_length / 100) * 100
 
-        self.parsed_gyro = np.zeros((self.num_gyro_samples, 4))
+        # self.parsed_gyro = np.zeros((self.num_gyro_samples, 4))
         # drift = -0.005832
         # offset = 0.048
         # self.parsed_gyro[:,0] = (np.arange(self.num_gyro_samples) * 1/self.gyro_rate - offset) * (1 - drift)
